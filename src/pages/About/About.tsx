3c8ce0bef48dd1b0900f76c37d958a9fe76aced4
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2023-08-22 16:39:38 +0300

    feat: bunner
    - add new component Bunner;
    - add style for Bunner.
    
    meta: issue-3

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 import { Description } from '../../components/About/Description';
 import { PlanTrip } from '../../components/About/PlanTrip';
+import { Bunner } from '../../components/Bunner/Bunner';
 
 export function About() {
   return (
-    <section className="about">
-      <Description />
-      <PlanTrip />
-    </section>
+    <>
+      <Bunner page="About" />
+
+      <section className="about">
+        <Description />
+        <PlanTrip />
+      </section>
+    </>
   );
 }
