af8c2ae9442fd797bddd247160a10adb8f3750e2
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2023-08-22 17:18:54 +0300

    feat: CallUs
    - add new component CallUs;
    - add style for CallUs.
    
    meta: issue-3

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Description } from '../../components/About/Description';
 import { PlanTrip } from '../../components/About/PlanTrip';
 import { Bunner } from '../../components/Bunner/Bunner';
+import { CallUs } from '../../components/CallUs/CallUs';
 
 export function About() {
   return (
@@ -13,6 +14,8 @@ export function About() {
         <Description />
         <PlanTrip />
       </section>
+
+      <CallUs />
     </>
   );
 }
